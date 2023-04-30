d40cfc601a314e4de0ec12555fdf78a894897c74
Alex Balcombe <alexbalcombe4@gmail.com>
2023-04-30 17:21:32 +0800

    Hope fix

--- src/routes/api/stripe/product/+server.ts
+++ src/routes/api/stripe/product/+server.ts
@@ -4,9 +4,6 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from '../../../../database.types';
 import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 
-const stripe = require('stripe')(
-	'sk_test_51N13N6Bl6RuBTaaaDXMmsEmBsF5OfOlcfg1xYkc4ndTsisZTLU2fJnzG0EogBR2Q0s7ibftgww3dZqZ6dK4fIo8b00WQ8c20pr'
-);
 
 const endpointSecret = 'whsec_WhNzKCBqkxQRrcvISMIKFRUlAuq6vObU';
 
@@ -16,24 +13,31 @@ const supabaseAdmin = createClient<Database>(
 );
 
 export const POST: RequestHandler = async ({ request }) => {
+	const stripe = require('stripe')(
+		'sk_test_51N13N6Bl6RuBTaaaDXMmsEmBsF5OfOlcfg1xYkc4ndTsisZTLU2fJnzG0EogBR2Q0s7ibftgww3dZqZ6dK4fIo8b00WQ8c20pr'
+	);
 	if (endpointSecret) {
-		const signature = request.headers.get('stripe-signature');
-		let receviedEvent;
-		try {
-			receviedEvent = await stripe.webhooks.constructEvent(request.body, signature, endpointSecret);
-		} catch (err) {
-			console.log(`⚠️  Webhook signature verification failed.`, err);
-			return json(400);
-		}
+			const signature = request.headers.get('stripe-signature');
+			let receviedEvent;
+			try {
+				receviedEvent = await stripe.webhooks.constructEvent(
+					request.body,
+					signature,
+					endpointSecret
+				);
+			} catch (err) {
+				console.log(`⚠️  Webhook signature verification failed.`, err);
+				return json(400);
+			}
 
-		switch (receviedEvent.type) {
-			case 'product.created':
-				const newProduct = receviedEvent.data.object;
-				// supabaseAdmin.from('products');
-				console.log('HI');
-				return json(200);
+			switch (receviedEvent.type) {
+				case 'product.created':
+					const newProduct = receviedEvent.data.object;
+					// supabaseAdmin.from('products');
+					console.log('HI');
+					return json(200);
+			}
 		}
-	}
 
 	return json(200);
 };
