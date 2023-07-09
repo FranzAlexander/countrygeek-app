c7bbdebb9d9795089793ff343469c30e47cfa14a
Alexander Balcombe <alexbalcombe4@gmail.com>
2023-07-09 12:38:29 +0800

    Remove the shop all together

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 
 <main
 	class=" flex h-full w-full flex-col gap-20 bg-fixed pb-10"
-	style="background-image: url('https://imagedelivery.net/cXH-XdNE8neFJ-7qoa3CXA/108164fc-2bf7-4fef-72e3-4d8832e6bc00/public'); background-repeat:no-repeat; "
+	style="background-image: url('https://imagedelivery.net/cXH-XdNE8neFJ-7qoa3CXA/108164fc-2bf7-4fef-72e3-4d8832e6bc00/public'); background-repeat:no-repeat;"
 >
 	<Hero />
 	<Feature services={data?.services ?? []} />
